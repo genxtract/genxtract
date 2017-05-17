cbb2af313fefce748df40cc01dc73048853b4a9b
John Clark <socrates37@gmail.com>
2017-05-17 13:37:29 -0600

    basic chrome extension; closes #12 (#13)
    * show active/inactive icon
    
    * use combinator and send info to background process
    
    * events and model being passed to output page
    
    * output events and data to page

--- packages/chrome-ext/src/background.js
+++ packages/chrome-ext/src/background.js
@@ -1,6 +1,54 @@
 import Extractors from '@genxtract/extract';
 
 const extractors = new Extractors();
+const outputURL = chrome.extension.getURL('output.html');
+
+let dataTab = null;
+let data = {};
+
+window.getData = function() {
+  return data;
+};
+
+function updateIcon(url) {
+  const {id} = extractors.match({url});
+  if (id) {
+    chrome.browserAction.setIcon({path: 'icons/active.svg'});
+  } else {
+    chrome.browserAction.setIcon({path: 'icons/inactive.svg'});
+  }
+}
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    updateIcon(tab.url);
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.active) {
+    updateIcon(tab.url);
+  }
+});
+
+// If our output page gets closed, update dataTab
+chrome.tabs.onRemoved.addListener((tabId) => {
+  if (dataTab === tabId) {
+    dataTab = null;
+  }
+});
+
+// Listen for a message when extraction is complete
+chrome.runtime.onMessage.addListener((message) => {
+  data = message;
+  if (dataTab === null) {
+    chrome.tabs.create({url: outputURL}, (tab) => {
+      dataTab = tab.id;
+    });
+  } else {
+    chrome.tabs.update(dataTab, {url: outputURL, active: true});
+  }
+});
 
 // Listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener((tab) => {
@@ -8,13 +56,12 @@ chrome.browserAction.onClicked.addListener((tab) => {
   const {id, path} = extractors.match({url: tab.url});
 
   if (id) {
-    // Inject event pipe
+    // Inject combinator
     chrome.tabs.executeScript(tab.id, {
-      file: 'events-to-console.js',
+      file: 'combinator.js',
     });
 
     // Inject extractor
-    console.log(`injecting ${id}, ${path}`);
     chrome.tabs.executeScript(tab.id, {
       file: path,
     });
