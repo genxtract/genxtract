c7a0a734738599d2762b514ecaa03d8b2d9f47a0
John Clark <socrates37@gmail.com>
2017-05-17 10:44:55 -0600

    show active/inactive icon

--- packages/chrome-ext/src/background.js
+++ packages/chrome-ext/src/background.js
@@ -2,6 +2,27 @@ import Extractors from '@genxtract/extract';
 
 const extractors = new Extractors();
 
+function updateIcon(url) {
+  const {id} = extractors.match({url});
+  if (id) {
+    chrome.browserAction.setIcon({path: 'icons/active.svg'});
+  } else {
+    chrome.browserAction.setIcon({path: 'icons/inactive.svg'});
+  }
+}
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    updateIcon(tab.url);
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.active) {
+    updateIcon(tab.url);
+  }
+});
+
 // Listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener((tab) => {
   // Get matching extractors
