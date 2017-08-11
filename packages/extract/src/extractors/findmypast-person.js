138d60c5eae19cb9cc4c3bf043bcac690826de10
Justin <justincyork@gmail.com>
2017-08-11 16:36:40 -0500

    persons and relationships

--- packages/extract/src/extractors/findmypast-person.js
+++ packages/extract/src/extractors/findmypast-person.js
@@ -43,7 +43,81 @@ function processData(treeId, personId, data) {
     id: personId,
   });
 
-  console.log(data);
+  const relations = new Relations(data);
+
+  // Emit persons
+  relations.getPersons().forEach((person) => {
+    const personId = person.Id;
+    emit.Name({
+      person: personId,
+      given: person.GivenNames,
+      surname: person.Surnames,
+    });
+    if(person.BirthDate || person.BirthPlace) {
+      emit.Birth({
+        person: personId,
+        date: convertDate(person.BirthDate),
+        place: person.BirthPlace,
+      });
+    }
+    if(person.DeathDate || person.DeathPlace) {
+      emit.Death({
+        person: personId,
+        date: convertDate(person.DeathDate),
+        place: person.DeathPlace,
+      });
+    }
+  });
+
+  // Spouses and Children
+  relations.getFamilies(personId).forEach((family) => {
+    const spouseId = family.FatherId === personId ? family.MotherId : family.FatherId;
+    const spouses = [personId, spouseId];
+    const marriage = {
+      spouses,
+    };
+    if(family.MarriageDate) {
+      marriage.date = convertDate(family.MarriageDate);
+    }
+    if(family.MarriagePlace) {
+      marriage.place = family.MarriagePlace;
+    }
+    emit.Marriage(marriage);
+    relations.getChildren(family.Id).forEach((child) => {
+      emit.Birth({
+        person: child.ChildId,
+        parents: spouses,
+      });
+    });
+  });
+
+  // Parents and Siblings
+  relations.getChildRefs(personId).map((child) => {
+    return relations.getFamily(child.FamilyId);
+  }).forEach((family) => {
+    const parents = [];
+    if(family.FatherId) {
+      parents.push(family.FatherId);
+    }
+    if(family.MotherId) {
+      parents.push(family.MotherId);
+    }
+    if(parents.length === 2) {
+      emit.Marriage({
+        spouses: parents,
+      });
+    }
+    emit.Birth({
+      person: personId,
+      parents,
+    });
+    relations.getChildren(family.Id).forEach((child) => {
+      emit.Birth({
+        person: child.ChildId,
+        parents,
+      });
+    });
+  });
 
   emit.Citation({
     title: document.title,
@@ -53,6 +127,27 @@ function processData(treeId, personId, data) {
     repository_website: window.location.hostname,
     repository_url: window.location.origin,
   });
+}
+
+/**
+ * Convert a findmypast date integer of form 19260504
+ * into a date string of form 1926-05-04.
+ * 
+ * @param {Integer} dateInt
+ * @return {String}
+ */
+function convertDate(dateInt) {
+  if(dateInt) {
+    const dateString = '' + dateInt;
+    const year = dateString.substr(0, 4);
+    const month = dateString.substr(4, 2);
+    const day = dateString.substr(6, 2);
+    if(month === '00' || day === '00') {
+      return year;
+    } else {
+      return `${year}-${month}-${day}`;
+    }
+  }
 }
 
 /**
@@ -82,4 +177,43 @@ async function api(treeId, url) {
   if(response.ok) {
     return await response.json();
   }
+}
+
+/**
+ * Class that simplifies access of ProfileRelations API response data
+ */
+class Relations {
+  
+  constructor(data) {
+    this.data = data;
+  }
+
+  getPersons() {
+    return this.data.Persons;
+  }
+
+  getFamily(familyId) {
+    return this.data.Familys.find(function(family) {
+      return family.Id === familyId;
+    });
+  }
+
+  getFamilies(personId) {
+    return this.data.Familys.filter(function(family) {
+      return family.FatherId === personId || family.MotherId === personId;
+    });
+  }
+
+  getChildren(familyId) {
+    return this.data.Childs.filter(function(child) {
+      return child.FamilyId === familyId;
+    });
+  }
+
+  getChildRefs(personId) {
+    return this.data.Childs.filter(function(child) {
+      return child.ChildId === personId;
+    });
+  }
+
 }
