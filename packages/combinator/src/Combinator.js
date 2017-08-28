32e5c24af27e37a78e383f6539634d861e1b46d4
Justin <justincyork@gmail.com>
2017-08-28 10:04:36 -0500

    Document Combinator; #74

--- packages/combinator/src/Combinator.js
+++ packages/combinator/src/Combinator.js
@@ -1,4 +1,16 @@
+/**
+ * The base class for all combinators. This handles the common handling of
+ * state and processing of genxtract events.
+ * 
+ * Classes which extend this class must implement the dataCallback() method.
+ * They may optionally implement the finalizeCallback() method.
+ */
 class Combinator {
+  
+  /**
+   * @param {Object=} data
+   * @param {Integer=} data.timeout Timeout, in seconds.
+   */
   constructor({timeout = 10} = {}) {
     this.__timeout = timeout; // The timeout, in seconds
     this.__timeoutId = null; // The timeout id returned by setTimeout
@@ -10,8 +22,12 @@ class Combinator {
     this.__completed = false; // If we have resolved or rejected
   }
 
+  /**
+   * Listen for genxtract events
+   * 
+   * @return {Promise} Resolves with the serialized data or rejects with an error.
+   */
   start() {
-    // Listen for genxtract events
     window.addEventListener('genxtract', this.__processEventHandler);
     // Return and store the promise
     return new Promise((resolve, reject) => {
@@ -20,6 +36,33 @@ class Combinator {
     });
   }
 
+  /**
+   * Process data from the extractor.
+   * 
+   * @abstract
+   * @param {Object} obj
+   * @param {String} obj.type Data type
+   * @param {Object} obj.data Data
+   */
+  dataCallback({type, data}) {
+    throw new Error('Method must be implemented by subclasses.');
+  }
+
+  /**
+   * Optional method that can be used for post-processing before data is serialized.
+   * 
+   * @abstract
+   */
+  finalizeCallback() {
+    throw new Error('Method must be implemented by subclasses.');
+  }
+
+  /**
+   * Process genxtract events
+   * 
+   * @param {Event} e
+   * @return {void}
+   */
   __processEvent(e) {
     // Do nothing if we have completed
     if (this.__completed) {
@@ -83,6 +126,12 @@ class Combinator {
     }
   }
 
+  /**
+   * Called when extraction is complete. Here we cleanup, serialize,
+   * and resolve the promise.
+   * 
+   * @param {Error=} error 
+   */
   __complete(error = null) {
     // Ignore if we already completed
     if (this.__completed) {
@@ -107,8 +156,11 @@ class Combinator {
     }
   }
 
+  /**
+   * Fire an error if extraction complete before the timeout
+   */
   __timedout() {
-    // If we're not done at this point, timeout
+    // Only throw an error if the timeout fires before we're done.
     if (!this.__completed) {
       this.__complete(new Error('Timed Out'));
     }
